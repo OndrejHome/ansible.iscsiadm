6000db6de40cd61e3a54db59983800fe9ee4d485
Ondrej Famera <ondrej@famera.cz>
2018-04-08 15:39:05 +0200

    major rewrite - detect the initiator WWN using fact collecting module iscsadm_facts - detect the target WWNs automatically and connect to them - improve README

---
+++ library/iscsiadm_facts.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+DOCUMENTATION = '''
+---
+module: iscsiadm_facts
+short_description: iscsiadm facts module
+description:
+     - Module for gathering configuration facts of iscsiadm for ansible.
+version_added: "2.4"
+options:
+notes:
+   - Tested on CentOS 7.2, 7.4
+requirements: [ ]
+author: "Ondrej Famera <ondrej-xa2iel8u@famera.cz>"
+'''
+
+EXAMPLES = '''
+- iscsiadm_facts
+
+'''
+
+from subprocess import call
+
+def main():
+        module = AnsibleModule(
+                argument_spec = dict(),
+                supports_check_mode=True
+        )
+
+        result = {}
+
+        try:
+            file = open('/etc/iscsi/initiatorname.iscsi','r')
+            text = file.readlines()[0].rstrip()
+            result['ansible_facts'] = {}
+            result['ansible_facts']['iscsi_initiator_name'] = text.split('=')[1]
+        except IOError as e:
+            result['ansible_facts'] = {}
+        except OSError as e:
+            module.fail_json(msg="Failed to open /etc/iscsi/initiatorname.iscsi file - %s" %(e) )
+        module.exit_json(**result)
+
+# import module snippets
+from ansible.module_utils.basic import *
+main()
